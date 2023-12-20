c2b6f071f42654726455935846ae1e93bd6f5932
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-12-20 10:20:59 -0300

    new

--- conta.py
+++ conta.py
@@ -4,6 +4,7 @@ class Conta:
     def __init__(self, numero,titular,saldo,limite):
         print("Construindo objeto...{}".format(self))
         #acessando o arquivo que o self já sabe
-        self.numero = 123
-        self.titular = "Gabriel"
-        self.saldo = 1000
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
